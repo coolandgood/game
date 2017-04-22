b68af18f191a877fb6972073dac63f731b3f1556
alex <16batesa@gmail.com>
2017-04-22 14:47:15 +0100

    initial

---
+++ source/Main.hx
@@ -0,0 +1,12 @@
+package;
+
+import flixel.FlxGame;
+import openfl.Lib;
+import openfl.display.Sprite;
+
+class Main extends Sprite {
+  public function new() {
+    super();
+    addChild(new FlxGame(0, 0, PlayState, 1, 60, 60, true));
+  }
+}
