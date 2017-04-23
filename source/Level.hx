dbb550717100c14720f31af46d5bf59ccb5da978
alex <16batesa@gmail.com>
2017-04-23 08:02:08 +0100

    Fix border and automagically generate it

--- source/Level.hx
+++ source/Level.hx
@@ -3,6 +3,7 @@ package;
 import flixel.tile.FlxTilemap;
 import flixel.FlxG;
 import flixel.group.FlxGroup;
+import flixel.math.FlxPoint;
 
 import flixel.addons.editors.tiled.TiledLayer;
 import flixel.addons.editors.tiled.TiledTileLayer;
@@ -26,7 +27,6 @@ class Level extends FlxGroup {
 
     var map = new TiledMap('assets/data/$level.tmx');
     var solidLayer: TiledTileLayer = cast map.getLayer('solid');
-    var borderLayer: TiledTileLayer = cast map.getLayer('border');
 
     tilemap = new FlxTilemap();
     tilemap.loadMapFromArray(solidLayer.tileArray,
@@ -35,15 +35,7 @@ class Level extends FlxGroup {
                              AssetPaths.tileset__png,
                              16, 16, 1);
     add(tilemap);
-
-    border = new FlxTilemap();
-    border.loadMapFromArray(borderLayer.tileArray,
-                             map.width,
-                             map.height,
-                             AssetPaths.tileset__png,
-                             16, 16, 1);
-    add(border);
-
+    add(genBorder(map.width + 3, map.height + 3));
 
     var objectsLayer: TiledObjectLayer = cast map.getLayer('objects');
     var objects: Array<TiledObject> = objectsLayer.objects;
@@ -53,11 +45,8 @@ class Level extends FlxGroup {
         player.setPosition(object.x, object.y);
     }
 
-    width = map.width * 16;
-    height = map.height * 16;
-
-    player.lvWidth = width - 32;
-    player.lvHeight = height - 32;
+    player.lvWidth = width = map.width * 16;
+    player.lvHeight = height = map.height * 16;
   }
 
   override public function update(elapsed: Float) {
@@ -68,5 +57,31 @@ class Level extends FlxGroup {
 
   override public function draw() {
     super.draw();
+  }
+
+  private function genBorder(width: Int, height: Int): FlxTilemap {
+    var borderData: Array<Int> = [];
+
+    for (y in 0...height) {
+      for (x in 0...width) {
+        if (y == height-1) borderData.push(6);
+        else if (x == width-1) borderData.push(6);
+        else if (y == 0 || y == height-2) borderData.push(2);
+        else if (x == 0) borderData.push(2);
+        else if (x == width-2) borderData.push(2);
+        else borderData.push(0);
+      }
+    }
+
+    border = new FlxTilemap();
+    border.loadMapFromArray(borderData,
+                            width,
+                            height,
+                            AssetPaths.tileset__png,
+                            16, 16, 1);
+
+    trace(border);
+    border.x = border.y = -16;
+    return border;
   }
 }
